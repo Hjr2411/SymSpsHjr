77a7d9ebb43c550d556ad9964ef8f64daf56aa7b
Helio Junior <87399421+Hjr2411@users.noreply.github.com>
2025-08-26 18:30:59 -0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+// Configuração do Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyAeCrURSs0TBXlYF3TKLi4q98VwrGaKe_Q",
+  authDomain: "spsch-849e5.firebaseapp.com",
+  databaseURL: "https://spsch-849e5-default-rtdb.firebaseio.com",
+  projectId: "spsch-849e5",
+  storageBucket: "spsch-849e5.firebasestorage.app",
+  messagingSenderId: "698967090558",
+  appId: "1:698967090558:web:978781fd27b86c36203f2f",
+  measurementId: "G-C5D3774P2G"
+};
+
+// Inicializar Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
